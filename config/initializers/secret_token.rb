4325fb78a8266041b02621e77ff1b676a52e7dff
Michael Liu <superinsky@gmail.com>
2012-01-24 23:13:25 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rsars::Application.config.secret_token = '094603365697015a063afe1e82d95c47fc382dd680aac4f8faea8277416b16edc31a389a3bd1380e55c016fb91445e572598bbe1535dbedb4458a8d4e30c6a1b'
